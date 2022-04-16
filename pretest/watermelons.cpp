a66aa4736b2d9350530d139ba06bfe2bb829272c
Nacnano <59221274+Nacnano@users.noreply.github.com>
2022-04-16 19:49:21 +0700

    Create watermelons.cpp

---
+++ pretest/watermelons.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    ios_base::sync_with_stdio(0), cin.tie(0);
+    int w;
+    cin >> w;
+    cout << (w%2==0 && w>=4 ? "YES" : "NO");
+    
+}
